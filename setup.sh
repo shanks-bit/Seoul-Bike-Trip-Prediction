b97bb83b74d11ef5fab4e0b527b6e4493b2b3342
shanks-bit <56055059+shanks-bit@users.noreply.github.com>
2021-11-17 22:07:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@ echo "\
 [general]\n\
 email = \"shashankdhanai82@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
